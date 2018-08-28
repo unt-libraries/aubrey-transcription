62d3f99170042ddcf356cbaf56a8206df9ab0628
Gio Gottardi <somexpert@gmail.com>
2018-08-28 10:47:39 -0500

    Add tox file and setup.py.

---
+++ setup.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='aubrey-transcription',
+    version='1.0.0',
+    packages=find_packages(exclude=['tests']),
+    description='Serves up transcriptions info for Aubrey.',
+    long_description='See the GitHub page for more information.',
+    include_package_data=True,
+    install_requires=[
+        'pypairtree==1.0.0',
+        'flask==1.0.2',
+    ],
+    zip_safe=False,
+    url='https://github.com/unt-libraries/aubrey-transcription',
+    author='University of North Texas Libaries',
+    author_email='mark.phillips@unt.edu',
+    license='BSD',
+    keywords=[
+        'flask',
+        'json',
+        'transcript',
+        'aubrey',
+        'app',
+        'webapp',
+    ],
+    classifiers=[
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
